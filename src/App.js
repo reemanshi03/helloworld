ef308755b6675daafbf50f3e8610e12c839c6204
reemanshi03 <reemanshi@setu.in>
2022-03-16 10:55:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          123456
+          1234
           Learn React with Reemanshi Chugh
         </a>
       </header>
